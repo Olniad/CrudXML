0d3d76a83034ac48c6858d701bbcfe44e205a925
DANILO_ANTUNES_MARTINS <danilo.martins333326@gmail.com>
2022-08-31 14:00:36 -0300

    Resumo do projeto

---
+++ CrudXML/Cliente.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Windows;
+
+namespace CrudXML
+{
+    public class Cliente
+    {
+        #region Atributos
+        private string _nome;
+        private string _email;
+        #endregion
+
+        #region Propriedades
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        #endregion
+    }
+
+    //estão sendo feitas a listagem de clientes e o metodo adicionar
+
+    public class Clientes
+    {
+        private List<Cliente> _clientes;
+        public Clientes()
+        {
+            this._clientes = new List<Cliente>();
+        }
+
+        public void Adicionar(Cliente cliente)
+        {
+            if (this._clientes.Count(c => c.Nome.Equals(cliente.Nome)) > 0)
+            {
+                throw new Exception("Já existe um cliente com este nome");
+            }
+            else
+            {
+                this._clientes.Add(cliente);
+            }
+        }
+        public void Remover(Cliente cliente)
+        {
+            this._clientes.Remove(cliente);
+        }
+        public List<Cliente> ListarTodos()
+        {
+            return this._clientes;
+        }
+        public void Salvar()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<Cliente>));
+            FileStream fs = new FileStream("H://segundotrimestre//Clientes.xml", FileMode.OpenOrCreate);
+            ser.Serialize(fs, this._clientes);
+            fs.Close();
+        }
+        public void Carregar()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<Cliente>));
+            FileStream fs = new FileStream("H://www//POO/SegundaEtapa/CrudXML"), FIleMode.OpenOrCreate);
+            try
+            {
+                this._clientes = ser.Deserialize(fs) as List<Cliente>;
+            }
+            catch (InvalidOperationException ex)
+            {
+                ser.Serialize(fs, this._clientes);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+    }
+
+}
---
+++ CrudXML/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CrudXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(9, 20);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(9, 70);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 36);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(215, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 86);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(215, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(12, 112);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(215, 22);
+            this.btnCadastrar.TabIndex = 4;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(251, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(353, 281);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 331);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
